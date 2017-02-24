e7af6f80bf39172ab607f7dbd69b34589aaa110b
joyfish869 <goodmorning869@126.com>
2017-02-24 16:34:30 +0800

    modify app

--- joy/src/main/java/test/joy/App.java
+++ joy/src/main/java/test/joy/App.java
@@ -10,6 +10,6 @@ public class App
     {
         System.out.println( "Hello World!" );
         
-        System.out.println( "Hello Joy!" );
+        System.out.println( "Hello Joy2!" );
     }
 }
