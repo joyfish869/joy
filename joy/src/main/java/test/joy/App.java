2aab35367f93f0ae937d7f666900bef70ec9f526
joyfish869 <goodmorning869@126.com>
2017-02-24 16:31:56 +0800

    init

---
+++ joy/src/main/java/test/joy/App.java
@@ -0,0 +1,15 @@
+package test.joy;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        
+        System.out.println( "Hello Joy!" );
+    }
+}
