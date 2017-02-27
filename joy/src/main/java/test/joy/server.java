565f6b60f66d9aac858e46d693003278e508c842
joyfish869 <goodmorning869@126.com>
2017-02-27 10:29:36 +0800

    add server

---
+++ joy/src/main/java/test/joy/server.java
@@ -0,0 +1,13 @@
+package test.joy;
+
+public class server {
+
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World, server!" );
+        
+        System.out.println( "Hello Joy server!" );
+        
+        System.out.println( "Hello Joy server2!" );
+    }
+}
